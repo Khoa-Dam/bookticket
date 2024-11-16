b6e5b1c6902eadf6d0490ef13870457380ea086d
KhoaND hoc code <damngockhoa0703@gmail.com>
2024-11-17 02:18:06 +0700

    Update Backend

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,17 @@
-const express = require("express");
-const app = express();
+const app = require("./middlewares/app");
+const connection = require("./config/database");
+const PORT = 3000; // Đặt port cho backend
+const router = require("./router/api");
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
-
-app.listen(3000, () => console.log("Server ready on port 3000."));
-
-module.exports = app;
+app.use('/api', router);
+(async () => {
+    try {
+        await connection.getConnection(); // Kiểm tra kết nối
+        console.log("Database connected successfully!");
+        app.listen(PORT, () => {
+            console.log(`Server is running on http://localhost:${PORT}`);
+        });
+    } catch (error) {
+        console.error("Error connecting to the database:", error.message);
+    }
+})();
